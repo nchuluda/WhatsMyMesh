438454ae25859e869ed4f7dba3a02d786a836eac
Nathan Chuluda <nchuluda@gmail.com>
2026-03-03 13:12:56 -0500

    Initial Commit

---
+++ WhatsMyMesh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsMyMesh
+//
+//  Created by Nathan on 3/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsMyMesh/WhatsMyMeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsMyMeshApp.swift
+//  WhatsMyMesh
+//
+//  Created by Nathan on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct WhatsMyMeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
